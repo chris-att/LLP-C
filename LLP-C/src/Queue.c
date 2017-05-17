a05c2c5c0312e82de747ff78e5f70666fa526e2d
Christian Attard <chriss.attard@gmail.com>
2017-05-17 17:28:34 +0000

    file_read_fix

---
+++ LLP-C/src/Queue.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "Queue.h"
+#define limit 600
+//-
+queue* loadQueue(queue* myQueue)
+{
+	free(myQueue);
+	myQueue = createQueue(limit);
+	int ch;
+	FILE* fp;
+	fp = fopen("/home/attac129/Documents/workspace/numbers", "r");
+	if (!fp) //fp is null report error
+	{
+		printf("Error opening file");
+	}
+	else //file opened without errors
+	{
+		while (fscanf(fp, "%d", &ch) == 1) //get ch from file
+		{
+			enque(myQueue, createNode(ch)); // display ch on screen
+		}
+	}
+	fclose(fp);
+	return myQueue;
+}
+
+void nextPlease(queue* myQueue)
+{
+	deque(myQueue);
+	getchar();
+}
+
+void addToQueue(queue* myQueue)
+{
+	int numToAdd;
+	printf("\nEnter a number to add to queue: ");
+	scanf("%d", &numToAdd);
+	if (enque(myQueue, createNode(numToAdd)) == false)
+	{
+		printf("\n We were unable to add extra items to the queue.");
+	}
+}
+
+void moreToServe(queue* myQueue)
+{
+	printf("\n%d more nodes to serve.", myQueue->size);
+	getchar();
+}
+
+void clearBuffer()
+{
+	int buffer = 0;
+	while ((buffer = getchar()) != '\n' && buffer != EOF)
+	{
+
+	}
+}
+
+void exitProgram()
+{
+	exit(0);
+}
+
+void menu(queue* myQueue)
+{
+	int inputQ1 = 0;
+	printf("\tMain Menu\n1. Load Queue\n2. Next Please!\n3. Add to Queue\n4. Show Queue\n5. How many more to serve?\n6. Exit\n\n Enter your choice: ");
+	scanf("%d", &inputQ1);
+	switch (inputQ1)
+	{
+	case 1: loadQueue(myQueue);
+		getchar();
+		system("clear");
+		menu(myQueue);
+	case 2: nextPlease(myQueue);
+		getchar();
+		system("clear");
+		menu(myQueue);
+	case 3: addToQueue(myQueue);
+		getchar();
+		system("clear");
+		menu(myQueue);
+	case 4: showQueue(myQueue);
+		getchar();
+		getchar();
+		system("clear");
+		menu(myQueue);
+	case 5: moreToServe(myQueue);
+		getchar();
+		system("clear");
+		menu(myQueue);
+	case 6: break; exitProgram();
+	default: printf("\nIncorrect choice .. now exiting.\n");
+		getchar();
+		getchar();
+		system("clear");
+		menu(myQueue);
+	}
+}
+
+int main(void)
+{
+	queue* myQueue = createQueue(limit);
+	myQueue = loadQueue(myQueue);
+	menu(myQueue);
+}
+
---
+++ LLP-C/src/Queue.h
@@ -0,0 +1,121 @@
+#pragma once
+typedef struct nodeRec
+{
+	int item;
+	struct nodeRec* next;
+} node;
+
+typedef node* nodePtr;
+
+typedef struct queue
+{
+	node* Head;
+	node* Tail;
+	int size;
+	int limit;
+} queue;
+
+queue* createQueue(int limit)
+{
+	queue* myQueue = (queue*)malloc(sizeof(queue));
+	myQueue->limit = limit;
+	myQueue->size = 0;
+	myQueue->Head = NULL;
+	myQueue->Tail = NULL;
+	return myQueue;
+}
+
+node* createNode(int item)
+{
+	node* newNode = (node*)malloc(sizeof(nodePtr));
+	newNode->item = item;
+	return newNode;
+}
+
+bool enque(queue* myQueue, node* newNode)
+{
+	if ((myQueue == NULL) || (newNode == NULL) || (myQueue->size >= myQueue->limit))
+	{
+		return false;
+	}
+	if (myQueue->size == 0)
+	{
+		myQueue->Head = newNode;
+		myQueue->Tail = newNode;
+		myQueue->size++;
+		return true;
+	}
+	else
+	{
+		myQueue->Tail->next = newNode;
+		myQueue->Tail = myQueue->Tail->next;
+		myQueue->Tail->next = NULL;
+		myQueue->size++;
+		return true;
+	}
+
+}
+
+bool isEmpty(queue* myQueue)
+{
+	if ((myQueue == NULL) || (myQueue->size != 0))
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool deque(queue* myQueue)
+{
+	int itemToReturn = 0;
+	if (isEmpty(myQueue) == true)
+	{
+		printf("\nThere are no nodes in the list.");
+		return false;
+	}
+	else
+	{
+		itemToReturn = myQueue->Head->item;
+		myQueue->Head = myQueue->Head->next;
+		myQueue->size--;
+		printf("\nThe next node is: %d", itemToReturn);
+		return true;
+	}
+}
+
+void showQueue(queue* myQueue)
+{
+	if (isEmpty(myQueue) == true)
+	{
+		printf("\nThere are no nodes in the list.");
+	}
+	else
+	{
+		int forLoop = myQueue->size;
+		printf("\nDisplaying queue ..");
+		for (int i = 0; i < forLoop; i++)
+		{
+			deque(myQueue);
+		}
+	}
+
+}
+
+int queueSize(queue* myQueue)
+{
+	if (myQueue->size > 0)
+	{
+		return myQueue->size;
+	}
+	else if (myQueue->size == 0)
+	{
+		return 0;
+	}
+	else if (myQueue == NULL)
+	{
+		return -1;
+	}
+}
